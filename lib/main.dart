fbfe440b0de25c4b804eb218c39372054d8dac7a
Numan <nomanuddin2012@gmail.com>
2022-07-23 22:45:59 +0500

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
 
 import 'package:flutter/material.dart';
-import 'package:flutter_quizzler/practice.dart';
 import 'package:flutter_quizzler/quiz_brain.dart';
 import 'package:rflutter_alert/rflutter_alert.dart';
 
